e92d914d1000c33abd007efa593e47699f7921c0
Kristi <kristigylm@gmail.com>
2021-01-23 21:33:47 +0100

    [WIP] Update mdl_to_vmdl.py

--- utils/mdl_to_vmdl.py
+++ utils/mdl_to_vmdl.py
@@ -1,21 +1,18 @@
 #!usr/bin/python
 from pathlib import Path
-import py_shared as sh
+import shared.base_utils as sh
 
 SHOULD_OVERWRITE = False
 
 IN_EXT = '.mdl'
 OUT_EXT = '.vmdl'
 
-print('Source 2 VMDL Generator!')
-
 fs = sh.Source("models")
-fileList = fs.collect_files(IN_EXT, OUT_EXT, SHOULD_OVERWRITE)
 
-for mdl_path in fileList:
+def ImportMDLtoVMDL_simple(mdl_path, move_s1_assets = False):
     vmdl_path = fs.Output(mdl_path).with_suffix(OUT_EXT)
     m_sMDLFilename = fs.LocalDir(mdl_path).as_posix()
-    fs.MakeDir(vmdl_path.parent)
+    vmdl_path.parent.MakeDir()
 
     print('Importing', m_sMDLFilename)
 
@@ -23,3 +20,15 @@ for mdl_path in fileList:
         out.write("<!-- kv3 encoding:text:version{e21c7f3c-8a33-41c5-9977-a76d3a32aa0d} format:generic:version{7412167c-06e9-4698-aff2-e63eb59037e7} -->\n{\r\n")
         out.write(f"\tm_sMDLFilename = \"{m_sMDLFilename}\"\r\n")
         out.write("}\r\n")
+
+def main():
+    
+    print('Source 2 VMDL Generator!')
+    
+    fileList = fs.collect_files(IN_EXT, OUT_EXT, SHOULD_OVERWRITE)
+
+    for mdl_path in fileList:
+        ImportMDLtoVMDL_simple(mdl_path)
+
+if __name__ == "__main__":
+    main()
