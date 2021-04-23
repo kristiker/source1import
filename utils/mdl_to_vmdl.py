2fa8b1dc8b0369bb3d37417438533e1ebb0a581a
Kristi K <kristigylm@gmail.com>
2021-04-23 17:46:31 +0200

    Update mdl_to_vmdl.py

--- utils/mdl_to_vmdl.py
+++ utils/mdl_to_vmdl.py
@@ -1,34 +1,27 @@
 #!usr/bin/python
-from pathlib import Path
-import shared.base_utils as sh
+from shared.base_utils import Source
 
 SHOULD_OVERWRITE = False
 
-IN_EXT = '.mdl'
-OUT_EXT = '.vmdl'
+fs = Source("models")
 
-fs = sh.Source("models")
-
-def ImportMDLtoVMDL_simple(mdl_path, move_s1_assets = False):
-    vmdl_path = fs.Output(mdl_path).with_suffix(OUT_EXT)
-    m_sMDLFilename = fs.LocalDir(mdl_path).as_posix()
+def ImportMDLtoVMDL(mdl_path, move_s1_assets = False):
+    vmdl_path = fs.Output(mdl_path).with_suffix('.vmdl')
+    mdl_filename = fs.LocalDir(mdl_path).as_posix()
     vmdl_path.parent.MakeDir()
 
-    print('Importing', m_sMDLFilename)
+    print('Importing', mdl_filename)
 
     with open(vmdl_path, 'w+') as out:
         out.write("<!-- kv3 encoding:text:version{e21c7f3c-8a33-41c5-9977-a76d3a32aa0d} format:generic:version{7412167c-06e9-4698-aff2-e63eb59037e7} -->\n{\r\n")
-        out.write(f"\tm_sMDLFilename = \"{m_sMDLFilename}\"\r\n")
+        out.write(f"\tm_sMDLFilename = \"{mdl_filename}\"\r\n")
         out.write("}\r\n")
 
-def main():
-    
+if __name__ == "__main__":
+
     print('Source 2 VMDL Generator!')
-    
-    fileList = fs.collect_files(IN_EXT, OUT_EXT, SHOULD_OVERWRITE)
+
+    fileList = fs.collect_files('.mdl', '.vmdl', SHOULD_OVERWRITE)
 
     for mdl_path in fileList:
-        ImportMDLtoVMDL_simple(mdl_path)
-
-if __name__ == "__main__":
-    main()
+        ImportMDLtoVMDL(mdl_path)
