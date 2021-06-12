1aa13c8a93a924f70b4c4c49119e5b0f99d4f73e
Kristi K <kristigylm@gmail.com>
2021-06-12 04:11:37 +0200

    Add base_utils replacement. Use base_utils2 on models_import.py

--- utils/models_import.py
+++ utils/models_import.py
@@ -1,27 +1,28 @@
 #!usr/bin/python
-from shared.base_utils import Source
+import shared.base_utils2 as sh
 
 SHOULD_OVERWRITE = False
 
-fs = Source("models")
-
+#@sh.s1import('.vmdl')
+#def ImportMDLtoVMDL(mdl_path, vmdl_path, move_s1_assets = False):
 def ImportMDLtoVMDL(mdl_path, move_s1_assets = False):
-    vmdl_path = fs.Output(mdl_path).with_suffix('.vmdl')
-    mdl_filename = fs.LocalDir(mdl_path).as_posix()
-    vmdl_path.parent.MakeDir()
 
-    print('Importing', mdl_filename)
+    vmdl_path = sh.output(mdl_path, '.vmdl')
 
-    with open(vmdl_path, 'w+') as out:
+    with open(vmdl_path, 'w') as out:
         out.write("<!-- kv3 encoding:text:version{e21c7f3c-8a33-41c5-9977-a76d3a32aa0d} format:generic:version{7412167c-06e9-4698-aff2-e63eb59037e7} -->\n{\r\n")
-        out.write(f"\tm_sMDLFilename = \"{mdl_filename}\"\r\n")
+        out.write(f"\tm_sMDLFilename = \"{mdl_path.local.as_posix()}\"\r\n")
         out.write("}\r\n")
+    
+    print('+ Generated', vmdl_path.local)
+    return vmdl_path
 
 if __name__ == "__main__":
-
     print('Source 2 VMDL Generator!')
 
-    fileList = fs.collect_files('.mdl', '.vmdl', SHOULD_OVERWRITE)
+    mdl_files = sh.collect('models', '.mdl', '.vmdl', SHOULD_OVERWRITE)
 
-    for mdl_path in fileList:
-        ImportMDLtoVMDL(mdl_path)
+    for mdl in mdl_files:
+        ImportMDLtoVMDL(mdl)
+    
+    print("Looks like we are done!")
---
+++ utils/shared/base_utils2.py
@@ -0,0 +1,148 @@
+
+from dataclasses import dataclass
+from pathlib import Path
+from enum import Enum
+from types import GeneratorType
+from typing import Iterable
+
+
+IMPORT_CONTENT = None
+IMPORT_GAME = None
+EXPORT_GAME = None
+EXPORT_CONTENT = None
+
+IMPORT_CONTENT =    Path(r'D:\Games\steamapps\common\Half-Life Alyx\content\csgo')
+IMPORT_GAME =       Path(r'D:\Games\steamapps\common\Half-Life Alyx\game\csgo')
+EXPORT_CONTENT =    Path(r'D:\Games\steamapps\common\Half-Life Alyx\content\hlvr_addons\csgo')
+EXPORT_GAME =       Path(r'D:\Games\steamapps\common\Half-Life Alyx\game\hlvr_addons\csgo')
+
+importing = Path()
+
+import_context = {
+    'mod': None,
+    'recurse': True,
+    'importfunc': 'integ',
+    'src': IMPORT_GAME,
+    'dest': EXPORT_CONTENT,
+    'ignoresource2namefixup': False,
+    'getSkinningFromLod0': False,
+}
+_mod = lambda: import_context['mod']
+_recurse = lambda: import_context['recurse']
+_src = lambda: import_context['src']
+_dest = lambda: import_context['dest']
+
+class Importable:
+    src: Path = import_context['src']
+    dest: Path = import_context['dest']
+    _currentpath: Path = None
+    params: dict
+
+    @property
+    def path(self):
+        return self._currentpath
+
+from functools import wraps
+def add_property(cls):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        setattr(cls, func.__name__, property(fget=wrapper))
+        return func
+    return decorator
+
+@add_property(Path)
+def local(self):
+    try: return self.relative_to(_src())
+    except ValueError:
+        return self.relative_to(import_context['dest'])
+
+def output(input, out_ext=None, dest=_dest()):
+    out = dest / input.local
+    if out_ext is not None:
+        return out.with_suffix(out_ext)
+    return out
+
+def s1import(out_ext=None, **ctx):
+    if not ctx: ctx = import_context
+    def inner_function(function):
+        @wraps(function)
+        def wrapper(asset_in: Path, asset_out: Path = None, **kwargs):
+            for k in kwargs.copy():
+                if k in import_context:
+                    ctx[k] = kwargs[k]
+                    kwargs.pop(k)
+            if asset_out is None:
+                asset_out = output(asset_in, out_ext, ctx['dest'])
+            asset_out.parent.mkdir(parents=True, exist_ok=True)
+            rv = function(asset_in, asset_out, **kwargs)
+            return rv
+        return wrapper
+    return inner_function
+
+from json import load as load_json
+def _get_blacklist(root):
+    json_path = Path(__file__).parent / "import_blacklist.json"
+    try:
+        with open(json_path, "r") as fp:
+            return load_json(fp).get(str(root), set())
+    except Exception as ex:
+        print(type(ex), "COULDNT GET BLACKLIST", json_path)
+        return set()
+
+
+def collect(root, inExt, outExt, existing:bool = False, outNameRule = None, searchPath = None, match = None, skiplist = None):
+    if not isinstance(outExt, (set, tuple, list)):
+        outExt = ((outExt),) # support multiple output extensions
+
+    #files_with_ext = []
+    if searchPath is None:  searchPath = (_src() / root)
+    if skiplist is None:    skiplist = _get_blacklist(root)
+
+    if searchPath.is_file():
+        if searchPath.suffix == inExt:
+            yield searchPath
+        else: print(f"~ File suffix is not a {inExt}")
+
+    elif searchPath.is_dir():
+        skipCountExists, skipCountBlacklist = 0, 0
+        print(f'\n- Searching %sfor%s %s files...' % (
+            "non-recursively "*(not _recurse()),
+            " unimported"*(not existing),
+            f"[ {match} ]" if match else inExt,
+        ))
+        if match is None:
+            match = ('**/'*_recurse()) + '*' + inExt  
+
+        for filePath in searchPath.glob(match):
+            bSkipThis = False
+            if outNameRule:
+                possibleNameList = outNameRule(filePath)
+            else: possibleNameList = filePath
+            if not isinstance(possibleNameList, (list, GeneratorType)): possibleNameList = [possibleNameList] # support multiple output names
+            #try:
+            # Attempt to see if you have an iterable object.
+            for filePath2 in possibleNameList: # try a number of possible outputs. default is list() which will give one output
+                if bSkipThis: break
+                for outExt_ in outExt:
+                    if bSkipThis: break
+                    if not existing and output(filePath2, outExt_, import_context['dest']).exists():
+                        skipCountExists += 1
+                        bSkipThis = True
+            #except TypeError:
+                # some_thing_which_may_be_a_generator isn't actually a generator
+                # do something else
+
+            for skip_match in skiplist:
+                if bSkipThis: break
+                if (skip_match.replace("\\", "/") in filePath2.as_posix()) or filePath2.match(skip_match):
+                    skipCountBlacklist += 1
+                    bSkipThis = True
+
+            if bSkipThis: continue #del files_with_ext[files_with_ext.index(filePath)]
+            yield filePath
+
+        print(' '*4 + f"Skipped: " + f"{skipCountExists} already imported | "*(not existing) +\
+                                 f"{skipCountBlacklist} found in blacklist"
+        )
