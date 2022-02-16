69a4fa4e4a7771b648de72733236a8f5b4a913be
kristiker <kristigylm@gmail.com>
2022-02-16 23:03:12 +0100

    Update elements_import.py

--- utils/elements_import.py
+++ utils/elements_import.py
@@ -5,18 +5,45 @@ from pathlib import Path
 SHOULD_OVERWRITE = False
 
 def ImportSFMSession(session_path: Path):
+    """Update SFM resource references for S2FM."""
     sh.status(f"- Opening {session_path.local}...")
     try:
         session = dmx.load(session_path)
     except Exception:
         return print("Error while reading:", session_path.local)
 
-    #print(session.find_elements(elemtype='DmeFilmClip').pop()['materialOverlay'], "   ")
-    dme_models = session.find_elements(elemtype='DmeGameModel')
-    if dme_models is not None:
+    # Map
+    if (dme_clips := session.find_elements(elemtype='DmeFilmClip')) is not None:
+        for clip in dme_clips:
+            clip['mapname'] = clip.get('mapname', '').replace('.bsp', '.vmap')
+
+    # Materials
+    if (dme_overlays := session.find_elements(elemtype='DmeMaterialOverlayFXClip')) is not None:
+        for overlay in dme_overlays:
+            overlay['material'] = overlay.get('material', '').replace('.vmt', '.vmat')
+
+    # Models
+    if (dme_models := session.find_elements(elemtype='DmeGameModel')) is not None:
         for game_model in dme_models:
-            # TODO: source2namefixup
             game_model['modelName'] = game_model.get('modelName', '').replace('.mdl', '.vmdl')
+
+    # Particles
+    if (dme_particles := session.find_elements(elemtype='DmeGameParticleSystem')) is not None:
+        for game_particle in dme_particles:
+            game_particle['particleSystemType'] = sh.RemapTable.get('vpcf', {}).get(game_particle.get('particleSystemType', ''), '')
+
+    # Projected Lights (cookies)
+    if (dme_lights := session.find_elements(elemtype='DmeProjectedLight')) is not None:
+        for projected_light in dme_lights:
+            projected_light['texture'] = projected_light.get('texture', '').replace('.vtf', '.vtex')
+
+    # Sounds
+    if (dme_sounds := session.find_elements(elemtype='DmeGameSound')) is not None:
+        for game_sound in dme_sounds:
+            game_sound.name = game_sound.name.replace('\\', '/')
+            file = Path(game_sound.get('soundname', '')) # 'sounds'/ 
+            if file.name:
+                game_sound['soundname'] = file.with_suffix('.vsnd').as_posix()
 
     session_out_path = sh.output(session_path, dest=sh.EXPORT_GAME)
     session_out_path.parent.MakeDir()
