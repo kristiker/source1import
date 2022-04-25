8f456e28c71f993c91cead1e23c17826535f4b67
kristiker <kristik06@outlook.com>
2022-04-25 23:57:09 +0200

    Initial .sound

--- utils/scripts_import.py
+++ utils/scripts_import.py
@@ -10,19 +10,9 @@ from shared.keyvalues1 import KV, VDFDict
 
 OVERWRITE_SCRIPTS = True
 
-HLVR_ADDON_WRITE = False
-"""
-For hlvr_addons:
-    Set to True to also write sound event imports to `soundevents/{addon}_soundevents.vsndevts` FILE
-    The addon system demands all addon sounds be in this one file.
-
-https://developer.valvesoftware.com/wiki/Half-Life:_Alyx_Workshop_Tools/Addon_Sounds#Sound_Events_files
-"""
-
 scripts = Path('scripts')
 SOUNDSCAPES_MANIFEST = scripts / "soundscapes_manifest.txt"
 SURFACEPROPERTIES_MANIFEST = scripts / "surfaceproperties_manifest.txt"
-SOUND_OPERATORS_FILE = scripts / "sound_operator_stacks.txt" # TODO.....
 
 def main():
     sh.import_context['dest'] = sh.EXPORT_GAME
@@ -42,16 +32,12 @@ def main():
     # game sounds...
     sh.import_context['dest'] = sh.EXPORT_CONTENT
 
-    # 'scripts' 'soundevents' hybrid base_utils2 FIXME
     for file in (sh.src(scripts)).glob('**/game_sounds*.txt'):
         if file.name != 'game_sounds_manifest.txt':
-            ImportGameSound(file)
+            ImportGameSounds(file)
 
     if (boss:=sh.src(scripts)/'level_sounds_general.txt').is_file():
-        ImportGameSound(boss)
-
-    if HLVR_ADDON_WRITE:
-        print("Fix the misplaced brace ({) inside addon soundevent file or it won't compile.")
+        ImportGameSounds(boss)
 
     # surfaceproperties...
     manifest_handle = VsurfManifestHandler()
@@ -116,100 +102,7 @@ def ImportSoundscapeManifest(asset_path: Path):
     print("+ Saved manifest file", out_manifest.local)
     return out_manifest
 
-# in soundscapes_*.txt: soundmixer <string>
-# ->
-# Selects a custom soundmixer. Soundmixers manage the priority and volume of groups of sounds; create new ones in scripts\soundmixers.txt (ALWAYS use Default_Mix as a template).
-# 
-# "quiet"
-# {
-# 	"soundmixer"	"Citadel_Dialog_Only"
-# 
-# 	...
-# }
-
 from shared.keyvalues3 import dict_to_kv3_text
-
-collected = {}
-def collect_dev(k, v):
-    
-    v_evauluated = eval(repr(v))
-    v_type = type(v_evauluated)
-
-    c = collected.setdefault(k, ([], []))
-    if v_type not in c[0]:
-        c[0].append(v_type)
-        c[1].append(v_evauluated)
-
-CHAN = {
-    'CHAN_AUTO': 0,
-    'CHAN_WEAPON': 1,
-    'CHAN_VOICE': 2,
-    'CHAN_ITEM': 3,
-    'CHAN_BODY': 4,
-    'CHAN_STREAM': 0,#5,		# allocate stream channel from the static or dynamic area
-    'CHAN_STATIC': 0,#6,		# allocate channel from the static area 
-}
-SNDLVL = {
-    'SNDLVL_NONE': 0,
-    'SNDLVL_25dB': 25,
-    'SNDLVL_30dB': 30,
-    'SNDLVL_35dB': 35,
-    'SNDLVL_40dB': 40,
-    'SNDLVL_45dB': 45,
-    'SNDLVL_50dB': 50,
-    'SNDLVL_55dB': 55,
-    'SNDLVL_IDLE': 60,
-    'SNDLVL_TALKING': 60,
-    'SNDLVL_60dB': 60,
-    'SNDLVL_65dB': 65,
-    'SNDLVL_STATIC': 66,
-    'SNDLVL_70dB': 70,
-    'SNDLVL_NORM': 75,
-    'SNDLVL_75dB': 75,
-    'SNDLVL_80dB': 80,
-    'SNDLVL_85dB': 85,
-    'SNDLVL_90dB': 90,
-    'SNDLVL_95dB': 95,
-    'SNDLVL_100dB': 100,
-    'SNDLVL_105dB': 105,
-    'SNDLVL_120dB': 120,
-    'SNDLVL_130dB': 130,
-    'SNDLVL_GUNFIRE': 140,
-    'SNDLVL_140dB': 140,
-    'SNDLVL_150dB': 150,
-}
-PITCH = {
-    'PITCH_NORM': 100,
-    'PITCH_LOW': 95,
-    'PITCH_HIGH': 120,
-}
-
-import wave
-import contextlib
-def _get_wavfile_duration(wave_path):
-    with contextlib.closing(wave.open(wave_path,'r')) as f:
-        frames = f.getnframes()
-        rate = f.getframerate()
-        return frames / float(rate)
-
-def _handle_range(k, v):
-    if not (type(v) is str and ',' in v):
-        return
-    try:
-        mm = tuple(v.split(',', 1))
-        min, max = float(mm[0]), float(mm[1])
-    except Exception: return
-    else:
-        rv = {}
-        out_v = min+max / 2
-        range = out_v - min
-        if k == 'pitch':  # Normalize pitch
-            range=range/100;out_v=out_v/100
-        rv[k] = out_v
-        if range != 0:
-            rv[k + '_rand_min'] = -range
-            rv[k + '_rand_max'] = range
-        return rv
 
 """
 channel ['CHAN_VOICE']
@@ -217,120 +110,78 @@ volume [1, 0.3, '0.4, 0.7']
 soundlevel ['SNDLVL_NORM', 0]
 pitch ['PITCH_NORM', 150]
 wave ['common/null.wav']
-soundentry_version [2]
-operator_stacks [VDFDict([('start_stack', VDFDict([('import_stack', 'CS_limit_start')])), ('update_stack', VDFDict([('import_stack', 'CS_update_foley'), ('mixer', VDFDict([('mixgroup', 'FoleyWeapons')]))]))])]
-compatibilityattenuation [1.0]
 rndwave [VDFDict([('wave', '~player/footsteps/slosh1.wav'), ('wave', '~player/footsteps/slosh2.wav'), ('wave', '~player/footsteps/slosh3.wav'), ('wave', '~player/footsteps/slosh4.wav')])]
-precache_file ['scripts/game_sounds_fbihrt.txt']
-gamedata [VDFDict([('priority', 'Interesting')])]
-preload_file ['scripts/game_sounds_radio.txt']
-autocache_file ['sound/music/valve_csgo_01/game_sounds_music.txt']
-delay_msec [70]
-ignore_occlusion [1]
-alert [1]
-hrtf_follow [1]
 """
-
-op_stacks = {}
-
-def ImportGameSound(asset_path: Path):
-    "scripts/game_sounds*.txt -> soundevents/game_sounds*.vsndevts"
-    
-    vsndevts_file = sh.EXPORT_CONTENT / "soundevents" / asset_path.local.relative_to(scripts).with_suffix('.vsndevts')
-    vsndevts_file.parent.MakeDir()
-
-    if not OVERWRITE_SCRIPTS and vsndevts_file.exists():
-        return vsndevts_file
-    
+def ImportGameSounds(asset_path: Path):
+    "scripts/game_sounds*.txt -> [sounds/*/major.minor.sound]"
+    game_sound_folder = sh.EXPORT_CONTENT / "sounds" / asset_path.stem.removeprefix('game_sounds_')
+    game_sound_folder.MakeDir()
+    PITCH = {
+        'PITCH_NORM': 100,
+        'PITCH_LOW': 95,
+        'PITCH_HIGH': 120,
+    }
+    def _handle_range(k, v):
+        if not (type(v) is str and ',' in v):
+            return
+        try:
+            mm = tuple(v.split(',', 1))
+            min, max = float(mm[0]), float(mm[1])
+        except Exception:
+            return
+        else:
+            out_v = min+max / 2
+            range = out_v - min
+            if k == 'pitch':  # Normalize pitch
+                range=range/100;out_v=out_v/100
+            return out_v, range
     kv = KV.CollectionFromFile(asset_path)
-    kv3 = {}
-
-    for gamesound, gs_kv in kv.items():
-        
-        if gamesound[0].isdigit():
-            gamesound = '_' + gamesound
-
-        out_kv = dict(type='src1_3d')
-        for (i, k), v in gs_kv.items(indexed_keys=True):
-            out_k, out_v = k, v
-            collect_dev(k, v)
+    for gamesound, gs_data in kv.items(): # "weapon.fire", {}
+        sound_file = game_sound_folder / f'{gamesound}.sound'
+        sound_data = dict(
+            ui = False,
+            volume = 1.0,
+            volumerandom = 0.0,
+            pitch = 1.0,
+            pitchrandom = 0.0,
+            distancemax = 2000.0,
+            sounds = [],
+            selectionmode = "0",
+        )
+        if not OVERWRITE_SCRIPTS and sound_file.exists():
+            continue
+        for (_, k), v in gs_data.items(indexed_keys=True):
             ## Turns out you can have multiple 'wave' in counter strike global offensive!
-            # instead of using rndwave {} !! Fucking volvo
+            # instead of using rndwave {} !!
             if k == 'wave':
-                if i != 0:
-                    if i == 1:
-                        out_kv['vsnd_files'] = [out_kv['vsnd_files'], fix_wave_resource(v)]
-                    else:
-                        out_kv['vsnd_files'].append(fix_wave_resource(v))
+                if "common/null" in v:
                     continue
-                out_k, out_v = 'vsnd_files', fix_wave_resource(v)
-
-                if out_v == 'sounds/common/null.vsnd': continue
-    
+                sound_data['sounds'].append(fix_wave_resource(v))    
             elif k == 'rndwave':
-                out_k, out_v = 'vsnd_files', []
                 for rndwave_k, rndwave_v in v.items(indexed_keys=True):
                     if rndwave_k[1] != 'wave':
                         continue
                     res = fix_wave_resource(rndwave_v)
                     if res != 'sounds/common/null.vsnd':
-                        out_v.append(res)
-
-                if not out_v: continue
-    
-            #elif k == 'channel':  # big ass guess
-            #    out_k, out_v= 'event_type', float(CHAN.get(v, 0))
-
-            elif k in ('volume', 'pitch', 'soundlevel'):
-                if rangekv:=_handle_range(k, v):
-                    out_kv.update(rangekv)
+                        sound_data['sounds'].append(res)
+            elif k in ('volume', 'pitch'):
+                if range:=_handle_range(k, v):
+                    sound_data[k] = range[0]
+                    sound_data[k+'random'] = range[1]
                     continue
-
                 if k == 'volume':
-                    if v == 'VOL_NORM': out_v = 1.0  # aka just continue? (default)
-                    out_v = float(out_v)
+                    if v == 'VOL_NORM':
+                        continue
+                    sound_data[k] = float(v)
                 elif k == 'pitch':
                     if type(v) is str:
-                        out_v = PITCH.get(v, 100)
+                        v = PITCH.get(v, 100)
                     # Normalize pitch
-                    out_v = out_v / 100
-                elif k == 'soundlevel':
-                    if type(v) is str:
-                        if (out_v:=SNDLVL.get(v)) is None:
-                            out_v = 75
-                            if v.startswith('SNDLVL_'):
-                                try:
-                                    out_v = int(v[7:-2])
-                                except Exception:
-                                    print(v[7:])
-                            else: print(v)
-            elif k == 'delay_msec': out_k, out_v = 'delay', v/1000
-            elif k == 'ignore_occlusion': out_k, out_v = 'occlusion_scale', (1 if not v else 0)#'sa_enable_occlusion'
-            elif k == 'operator_stacks':  # this only exists in globul offensif
+                    sound_data[k] = v / 100
+            elif k == 'soundlevel':
                 ...
-                continue
-            elif k in ('soundentry_version', 'alert', 'hrtf_follow','gamedata',): # skiplist
-                continue
-            else:
-                continue
-            
-            out_kv[out_k] = out_v
-        if out_kv == dict(type='src1_3d'):  # empty
-            out_kv = None
-        kv3[gamesound] = out_kv
-
-    if HLVR_ADDON_WRITE:# and sh.is_addon():
-        addon_vsndevts = sh.EXPORT_CONTENT / "soundevents" / f"{sh.EXPORT_CONTENT.name}_soundevents.vsndevts"
-        if not addon_vsndevts.is_file():
-            sh.write(dict_to_kv3_text({}),addon_vsndevts)
-        with open(addon_vsndevts, 'a') as fp:
-            """Bad way of writing, manual fixing is necessary."""
-            fp.write('\n' + '\n'.join(dict_to_kv3_text(kv3).splitlines()[2:-1]))
-
-    sh.write(dict_to_kv3_text(kv3), vsndevts_file)
-
-    print("+ Saved", vsndevts_file.local)
-    return vsndevts_file
+        sh.write(dict_to_kv3_text(dict(data=sound_data)), sound_file)
+        print("+ Saved", sound_file.local)
 
 vsurf_base_params = {
     'physics': ('density','elasticity','friction','dampening','thickness',),
