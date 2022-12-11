2e009881f8a0d9b359b4e5c9fab950c0f3d48bab
kristiker <kristik06@outlook.com>
2022-12-11 15:32:48 +0100

    altered options for test files

--- tests/import_results_update.bat
+++ tests/import_results_update.bat
@@ -1,12 +1,27 @@
 
 echo hlvr
-del /s /q "%cd%/source2_hlvr_game"
-python ../utils/scripts_import.py -i "source_game" -e "%cd%/source2_hlvr_game" --branch hlvr
+rem del /s /q "%cd%/source2_hlvr_game"
+python ../utils/scripts_import.py -i "source_game" -e "%cd%/source2_hlvr_game" ^
+    --branch hlvr ^
+    OVERWRITE_ASSETS=True ^
 
 echo sbox
-del /s /q "%cd%/source2_sbox_game/"
-python ../utils/scripts_import.py -i "source_game" -e "%cd%/source2_sbox_game" --branch sbox
+rem del /s /q "%cd%/source2_sbox_game/"
+python ../utils/scripts_import.py -i "source_game" -e "%cd%/source2_sbox_game" ^
+    --branch sbox ^
+    OVERWRITE_ASSETS=True ^
+    SOUNDSCAPES=True ^
+    GAMESOUNDS=True ^
+    SURFACES=True ^
+    MISCELLANEOUS=False
 
-rem echo dota2
+echo adj
 rem del /s /q "%cd%/source2_adj_game"
-rem python ../utils/scripts_import.py -i "source_game" -e "%cd%/source2_adj_game" --filter "propdata"
+python ../utils/scripts_import.py -i "source_game" -e "%cd%/source2_adj_game" ^
+    --branch adj ^
+    OVERWRITE_ASSETS=True ^
+    SOUNDSCAPES=False ^
+    GAMESOUNDS=False ^
+    SURFACES=False ^
+    MISCELLANEOUS=True
+
