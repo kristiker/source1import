93f542941eecd16dacfe18a595e7e4d22e42c780
kristiker <kristik06@outlook.com>
2022-12-11 13:25:44 +0100

    Update import_results_update.bat

--- tests/import_results_update.bat
+++ tests/import_results_update.bat
@@ -1,11 +1,11 @@
 
 echo hlvr
 del /s /q "%cd%/source2_hlvr_game"
-python ../utils/scripts_import.py -i "source_game" -e "%cd%/source2_hlvr_game"
+python ../utils/scripts_import.py -i "source_game" -e "%cd%/source2_hlvr_game" --branch hlvr
 
 echo sbox
 del /s /q "%cd%/source2_sbox_game/"
-python ../utils/scripts_import.py -i "source_game" -e "%cd%/source2_sbox_game"
+python ../utils/scripts_import.py -i "source_game" -e "%cd%/source2_sbox_game" --branch sbox
 
 rem echo dota2
 rem del /s /q "%cd%/source2_adj_game"
