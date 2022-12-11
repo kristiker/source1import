eb39e850b7ec82bebf94edf53e39bc1d9b8d0335
kristiker <kristik06@outlook.com>
2022-12-11 16:41:35 +0100

    Replace batch file with python script

---
+++ tests/import_results_update.py
@@ -0,0 +1,62 @@
+import sys, os
+from types import ModuleType
+from typing import Any, Callable
+from pathlib import Path
+
+os.chdir(Path(__file__).parent)
+sys.path.append("../utils")
+
+import functools
+def workflow(*modules: tuple[ModuleType, dict[str, Any]]):
+    def inner(f: Callable):
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            for module, options in modules:
+                module.sh.update_destmod(module.sh.eS2Game(f.__name__))
+                module.sh.args_known.src1gameinfodir = "source_game"
+                module.sh.parse_in_path()
+                module.sh.parse_out_path(Path(f"source2_{f.__name__}_game").resolve())
+                for name, value in options.items():
+                    if hasattr(module, name):
+                        setattr(module, name, value)
+                f(module)
+        return wrapper
+    return inner
+
+import scripts_import
+
+@workflow(
+    (scripts_import, {
+        "OVERWRITE_ASSETS": True
+    }),
+)
+def hlvr(module: ModuleType):
+    module.main()
+
+@workflow(
+    (scripts_import, {
+        "OVERWRITE_ASSETS": True,
+        "SOUNDSCAPES": True,
+        "GAMESOUNDS": True,
+        "SURFACES": True,
+        "MISCELLANEOUS": False,
+    }),
+)
+def sbox(module: ModuleType):
+    module.main()
+
+@workflow(
+    (scripts_import, {
+        "OVERWRITE_ASSETS": True,
+        "SOUNDSCAPES": False,
+        "GAMESOUNDS": False,
+        "SURFACES": False,
+        "MISCELLANEOUS": True,
+    }),
+)
+def adj(module: ModuleType):
+    module.main()
+
+hlvr()
+sbox()
+adj()
